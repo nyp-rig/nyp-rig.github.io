78fc47a551efecb29e740ef35b705fad3e805e29
AzlanCoding <101229146+AzlanCoding@users.noreply.github.com>
2025-07-20 15:38:58 +0800

    Finish Landing Page

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,57 @@
 // Import all plugins
 import * as bootstrap from 'bootstrap';
 import $, { event } from 'jquery';
+
+
+function pageStartupCompleted(){
+  // Setup IntersectionObserver
+  const onIntersect = (objects, observer)=>{
+    for (let i = 0; i < objects.length; i++){
+      if (objects[i].isIntersecting){
+        objects[i].target.classList.add("is-in-view");
+      }else{
+        objects[i].target.classList.remove("is-in-view");
+      }
+    }
+  };
+  const observer = new IntersectionObserver(onIntersect, {threshold: 0.5});
+  const observer2 = new IntersectionObserver(onIntersect, {threshold: 0.2});
+
+  $('.section:not(.special)').each((_, elm)=>{
+    observer.observe(elm)
+  });
+  $('.section.special').each((_, elm)=>{
+    observer2.observe(elm)
+  })
+}
+
+
 $(() => {
+  //Handle Intro Animations
   if (document.getElementById('introScreen')){
-    $('html').addClass('is-ready');
-    setTimeout(()=>{
-      $('html').addClass('is-fully-ready');
-      setTimeout(()=>{// Remove element for firefox support.
-        $('#introScreen').remove()
-      }, 1000)
-    }, 2000)
+    setTimeout(()=> {
+      $('html').addClass('is-ready');
+      setTimeout(()=>{
+        $('html').addClass('is-fully-ready');
+        pageStartupCompleted();
+        setTimeout(()=>{// Remove element for firefox support.
+          $('#introScreen').remove()
+        }, 1000)
+      }, 2000)
+    }, 500)
   }
   else{
     $('html').addClass('is-ready');
     $('html').addClass('is-fully-ready');
+    pageStartupCompleted();
   }
+
+  // Register Loading Signs After Click
   $('.has-loading-aft-click').on('click', (event)=>{
     const elm = $(event.currentTarget);
     if (elm.attr('type') == 'submit' && !elm.parent()[0].checkValidity()){
       return;
     }
     elm.html(`<span class="spinner-border spinner-border-sm" aria-hidden="true"></span>`)
-    });   
+  });   
 });
